4c9243106a4fb28b5ba9e1bfb59e86c4de2196b0
argo2954 <109727039+argo2954@users.noreply.github.com>
2022-10-31 22:45:07 +0900

    API Key encryption

--- Back-End/Base.py
+++ Back-End/Base.py
@@ -8,8 +8,8 @@ from datetime import datetime
 class AccessToken :
     def __init__(self):
         self.starttime = datetime.now()
-        self.APP_KEY = "PSjTTTMhFyHdsLFfJm2CzSGMbLkBEF8nLKJW"
-        self.APP_SECRET = "PV+kbdrXdtuzA2IZmlmNRcmTeFFrX36c2R/nIA1k1ycKYFoNfos04y4wkeucqiOGAaOMWMnxvX/XQ1+Syd04d4mWPWSId7lJbX30YOTWN3NXUmouR4kAXwaBC42VhC+2TOIjQIcpRG7wAx9eUn+PS5Nb3LQrS4FMdOfjbOTQZ95ZC48SnD0="
+        self.APP_KEY = "App key"
+        self.APP_SECRET = "App Secret key"
         self.URL_BASE = "https://openapi.koreainvestment.com:9443" # 실전투자서비스
 
         self.headers = {"content-type":"application/json",
@@ -38,8 +38,8 @@ class DB :
     def __init__(self):
         self.db = pymysql.connect(
             # host='localhost' 가능
-            host='3.39.234.81', user='wc', 
-            password='root', port=3306, charset='utf8')
+            host='localhost', user='user name', 
+            password='password', port=3306, charset='utf8')
 
     def GetCursor(self):
         # DB cursor 반환
@@ -62,7 +62,7 @@ class DB :
 # DartKey 클래스
 class DartKey :
     def __init__(self):
-        self.api_key='6b077755950e89e2c8d1f705ee531c0374cd1da8'
+        self.api_key='api key'
     
     def GetApiKey(self):
         return self.api_key
